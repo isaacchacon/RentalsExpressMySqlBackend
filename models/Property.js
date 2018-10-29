82f87b406073e77ed08a0a4f2db5265bac56b794
a <isaacchacon@hotmail.com>
2018-10-29 17:03:53 -0500

    Commiting callback that synthetizes images into one array

--- models/Property.js
+++ models/Property.js
@@ -1,7 +1,7 @@
 var db = require('../dbconnection');
 var Property = {
 
-    allFields: `Property.idProperty,
+    allPropertyFields: `Property.idProperty,
     Property.name,
     Property.status,
     Property.address,
@@ -18,12 +18,39 @@ var Property = {
     Property.parkingDescription,
     Property.propertyGroup,
     Property.salesDescription,
-    Property.zipCode `,
+    Property.zipCode`,
+    allImageFields:"Images.imagePath, Images.order, Images.title, Images.description",
     getAllProperties:function(callback){
-        return db.query('Select '+this.allFields+' from Property where status ="available"', callback);
+        return db.query('Select '+this.allPropertyFields+' from Property where status ="available"', callback);
     },
+
     getPropertyById:function(id, callback){
-        return db.query('Select '+this.allFields+' from Property where idProperty=? and status ="available"', [id], callback);
+        return db.query('Select '+this.allPropertyFields+', '+this.allImageFields+' from Property  left join Images on Images.IdProperty = Property.idProperty where Property.idProperty=? and status ="available" order by Images.Order', [id], 
+        function(err, rows){
+            if(err||(!rows)||(!rows.length))
+            callback(err,rows);
+            else{
+                let finalResult ={}
+                //first populate external properties.
+                for (var k in rows[0]) {
+                    if((k=='order')||(k=='title'||k=='description'||k=='imagePath'))
+                    continue;
+                    if (!finalResult[k]) finalResult[k] = rows[0][k];
+                 }
+                finalResult.images = [];
+                const response = rows.reduce((acc, row) => {
+                    let imageObject = {};
+                    if(row.imagePath){
+                        imageObject.imagePath =row.imagePath;
+                        imageObject.title = row.title;
+                        imageObject.description = row.description;
+                        acc.images.push(imageObject);
+                    }
+                      return acc;  
+                }, finalResult);
+                callback(err, response);
+            }
+        });
     }
 }
 module.exports = Property;
